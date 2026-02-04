fb8928ab1e0d301e332667e43b8a70649cbd797c
SondosAhmed16 <www.sondossherif1234.com@gmail.com>
2026-02-05 00:11:50 +0200

    doctor can upload his member ship when regestering

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -11,7 +11,7 @@ const { runValidation } = require("../middleware/validate");
 
 // إعدادات Cloudinary
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
