ea306e085db13a87d2b0bcd92d67e7da43e7c0d7
SondosAhmed16 <www.sondossherif1234.com@gmail.com>
2026-05-04 01:51:18 +0300

    expire token

--- Backend/utils/tokens.js
+++ Backend/utils/tokens.js
@@ -4,7 +4,7 @@ exports.generateAccessToken = (user) => {
   return jwt.sign(
     { id: user._id, role: user.role },
     process.env.JWT_SECRET,
-    { expiresIn: "1h" }
+    { expiresIn: "1d" }
   );
 };
 
