10e2dc6e65e656104708f0906e0833f0722d8964
Haniin911 <hanin.shaaban22119@ci.menofia.edu.eg>
2026-05-03 12:31:58 +0300

    appointment cont2

--- Backend/routes/appointmentRoutes.js
+++ Backend/routes/appointmentRoutes.js
@@ -2,12 +2,11 @@ const express = require('express');
 const router = express.Router();
 const appointmentController = require('../Controllers/appointmentController');
 const { auth } = require('../middleware/auth');
-
 const { authorizeRoles } = require('../middleware/role');
 
-// ─── Appointments (patients only) ────────────────────────────────────────────
 router.post('/', auth, authorizeRoles('patient'), appointmentController.bookAppointment);
-router.get('/my', auth,authorizeRoles('patient'), appointmentController.getMyAppointments);
+router.get('/my', auth, authorizeRoles('patient'), appointmentController.getMyAppointments);
 router.patch('/:id/cancel', auth, authorizeRoles('patient'), appointmentController.cancelAppointment);
-router.patch("/:id/reschedule", auth, authorizeRoles('patient'), rescheduleAppointment);
+router.patch('/:id/reschedule', auth, authorizeRoles('patient'), appointmentController.rescheduleAppointment);
+
 module.exports = router;
