cca3e221e4a85cb21c6f2b365c8649925fbe325d
Haniin911 <hanin.shaaban22119@ci.menofia.edu.eg>
2026-06-17 22:00:41 +0300

    Doctor ai fix

--- Backend/routes/doctorRoutes.js
+++ Backend/routes/doctorRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const doctorController = require('../Controllers/doctorController');
-const aiDoctorController = require('../Controllers/aiDoctorController');
+const aiDoctorController = require('../Controllers/AiDoctorController');
 const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
